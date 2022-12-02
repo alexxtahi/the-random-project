9abadb945a4b45686b5011524ac9feece0f57236
Ptitruc <41187893+Ptitruc@users.noreply.github.com>
2022-12-02 02:48:58 +0000

    merge

--- jsfiles/buttons.js
+++ jsfiles/buttons.js
@@ -1,10 +1,10 @@
-const MAX_PROBLEM = 1;
+const MAX_PROBLEM = 2;
 
 const answer_buttons = document.getElementsByClassName("answer_button");
 const next_buttons = document.getElementsByClassName("next_buttons");
 const parent = document.getElementsByClassName("interactions")[0];
 const hud_bottom = document.getElementsByClassName("hud-bottom")[0];
-const hud_question = document.getElementsByName("question-box")[0];
+const hud_question = document.getElementsByClassName("patient-talk-box")[0];
 
 
 var previousProblems = []; // une liste des problemes qui ont été rencontré
@@ -14,6 +14,8 @@ var dataProblem;
 var etape_courante;
 var etape = 1;
 var dialogueEtape = 1;
+
+var correct;
 
 var score = 0;
 
@@ -26,45 +28,58 @@ if(xmlhttp.status==200){
 
 function loadProblem(id) {
     etape = 1;
-    dataProblem = bigdata["prob2"+id];
+    dataProblem = bigdata["prob"+id];
+    // console.log(dataProblem);
     loadStage(etape);
 }
 
 function loadStage(int) {
-
     const stages = dataProblem["etape"+int];
+    // console.log(stages);
     if (stages == undefined) return;
     else if (stages.type == "text"){
-        etape_courante = stages;
+        etape_courante = stages.text;
         dialogueEtape = 1;
+        // console.log(etape_courante["dialogue" + dialogueEtape]);
         showText(etape_courante["dialogue" + dialogueEtape]);
 
     }
     else if (stages.type == "choice"){
-        showButtons(stages.choices);
+        etape_courante = stages.choices;
+        correct = stages.correct;
+        showButtons(etape_courante, stages.correct);
     }
 }
 
 function nextDialogue() {
     dialogueEtape++;
-    if (!etape_courante["dialogue"] + dialogueEtape){
+    if (etape_courante["dialogue" + dialogueEtape ]  != undefined){
         clear();
-        showText(etape_courante["dialogue"] + dialogueEtape);
+        showText(etape_courante["dialogue" + dialogueEtape]);
+
+    }
+    else {
+        const next = document.getElementsByClassName("next")[0]; 
+        console.log(next);
+        hud_question.removeChild(next);
+        
+        etape++;
+        loadStage(etape);
     }
 }
 function showText(data){
-    if (data.doctor != undefined) {
+    if (data.docteur != undefined) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("question-box");
-        newDiv.textContent = data.doctor;
+        newDiv.textContent = data.docteur;
         
         const buttonNext = document.createElement("a");
-        buttonNext.classList.add("next_doctor");
+        buttonNext.classList.add("next");
         buttonNext.textContent= ">";
         buttonNext.addEventListener('click', () => nextDialogue() );
 
-        parent.appendChild(newDiv);
-        parent.appendChild(buttonNext);
+        hud_question.appendChild(newDiv);
+        hud_question.appendChild(buttonNext);
     }
     else if (data.patient != undefined) {
         const newDiv = document.createElement("div");
@@ -72,22 +87,22 @@ function showText(data){
         newDiv.textContent = data.patient;
         
         const buttonNext = document.createElement("a");
-        buttonNext.classList.add("next_patient");
+        buttonNext.classList.add("next");
         buttonNext.textContent= ">";
         buttonNext.addEventListener('click', () => nextDialogue() );
 
-        parent.appendChild(newDiv);
-        parent.appendChild(buttonNext);
+        hud_question.appendChild(newDiv);
+        hud_question.appendChild(buttonNext);
     }
 }
 
-function showButtons(data) {
-    const correctButton = data.correct;
+function showButtons(data, correctButton) {
     // data.splice(data.length()-1,1);
     for (obj in data) {
         if (obj == "correct") continue;
         const newDiv = document.createElement("a");
-        newDiv.classList.add("answer_box");
+        newDiv.classList.add("answer-box");
+        newDiv.id = obj;
         newDiv.textContent = data[obj];
         if (obj == correctButton){
             newDiv.addEventListener('click', ()=> positiveScore() );
@@ -95,26 +110,44 @@ function showButtons(data) {
         else {
             newDiv.addEventListener('click', ()=> negativeScore() );
         }
-        parent.appendChild(newDiv);
+        hud_bottom.appendChild(newDiv);
     }
 
+}
+function affichReponses(){
+    const listnodes = hud_bottom.childNodes;
+    console.log(listnodes); 
+    listnodes.forEach(e => {
+        console.log(e);
+        if (e.id == correct){
+            e.style.backgroundColor = "green";
+        } 
+        else {
+            e.style.backgroundColor = "red";
+        }
+    });
+    setTimeout(() => {
+    nextStage();
+    }, 2000);
 }
 
 function positiveScore() {
     score++;
     console.log(score);
-    nextStage();
+    
+    affichReponses();
 }
 function negativeScore() {
     score--;
     console.log(score);
-    nextStage();
+
+    affichReponses();
 }
 
 function nextStage(){
     clear();
     etape++;
-    if (dataProblem["etape"+int] == undefined) {
+    if (dataProblem["etape" + etape] == undefined) {
         randomProblem();
     }
     else {
@@ -123,19 +156,24 @@ function nextStage(){
 }
 
 function clear(){
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+    while (hud_bottom.firstChild) {
+        hud_bottom.removeChild(hud_bottom.firstChild);
+    }
+    while (hud_question.firstChild){
+        hud_question.removeChild(hud_question.firstChild);
     }
 }
 
 function randomProblem(){
-    const number = Math.random() * MAX_PROBLEM +1;
-    if (previousProblems.length == MAX_PROBLEM) {
+    var number = Math.round(Math.random() * (MAX_PROBLEM-1) ) + 1;
+    if (previousProblems.length == MAX_PROBLEM-1) {
         console.log("all problem solved"); //TODO
     }
     while (previousProblems.includes(number)) {
-        number = Math.random() * MAX_PROBLEM +1;
+        number = Math.round(Math.random() * (MAX_PROBLEM-1) ) + 1;
     }
+    console.log(number);
     clear();
     loadProblem(number);
+    previousProblems.push(number);
 }
